16ad5299f479c66530aad183dfc0b2893f28e00f
RaulSierro <raul.sierro@gmail.com>
2014-12-17 20:47:04 +0100

    Commit formulario mac

---
+++ orkiv/main.py
@@ -0,0 +1,11 @@
+__author__ = 'Raul'
+from  kivy.app import App
+from  kivy.uix.boxlayout import BoxLayout
+
+class Orkiv(App):
+    pass
+
+class FormaCuentaDetalles(BoxLayout):
+    pass
+
+Orkiv().run()
