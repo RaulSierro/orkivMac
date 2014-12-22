8f159dd369437a2004cc28a2d3680ce9f8734b3e
RaulSierro <raul.sierro@gmail.com>
2014-12-22 19:20:44 +0100

    BoxLayout en mac

--- orkiv/main.py
+++ orkiv/main.py
@@ -1,11 +1,21 @@
 __author__ = 'Raul'
 from  kivy.app import App
 from  kivy.uix.boxlayout import BoxLayout
+from  kivy.properties import ObjectProperty
+from  kivy.uix.anchorlayout import AnchorLayout
 
 class Orkiv(App):
     pass
 
-class FormaCuentaDetalles(BoxLayout):
-    pass
+class FormaCuentaDetalles(AnchorLayout):
+
+    server_box = ObjectProperty()
+    username_box = ObjectProperty()
+    password_box = ObjectProperty()
+    def login(self):
+        print(self.server_box.text)
+        print(self.username_box.text)
+        print(self.password_box.text)
+
 
 Orkiv().run()
